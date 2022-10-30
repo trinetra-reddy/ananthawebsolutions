a252f17537a74916b18437a5151a41ae3a77a356
Giri <gonuguntlagirish@gmail.com>
2022-10-30 22:09:41 +0530

    commited code

--- src/components/Ourteam/Ourteam.js
+++ src/components/Ourteam/Ourteam.js
@@ -5,37 +5,37 @@ import avatar2 from '../../images/avatar2.png';
 
 const Ourteam = () => {
   return (
-<div class="card-container">
+<div className="ourTeamcard-container">
       <h1>Our Creative Team</h1>
       <h2>Our Awesome Team</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis voluptates labore voluptate 
       exercitationem quos fugiat deleniti minus, aut voluptatem praesentium, suscipit animi. Obcaecati 
       asperiores tempore saepe recusandae labore praesentium!</p>
-      <div class="row">
-            <div class="column-container">
-              <div class="column">
-                <div class="card">
+      <div className="ourTeam-row">
+            <div className="ourTeam-column-container">
+              <div className="ourTeam-column">
+                <div className="ourTeam-card">
                   <img src={avatar} alt="photo-1"/>
                   <h3>Employe Name</h3>
                   <h4>Role/position</h4>
                 </div>
             </div>
-            <div class="column">
-                <div class="card">
+            <div className="ourTeam-column">
+                <div className="ourTeam-card">
                   <img src={avatar2} alt="photo-1"/>
                   <h3>Employe Name</h3>
                   <h4>Role/position</h4>
                 </div>
             </div>
-                <div class="column">
-                    <div class="card">
+                <div className="ourTeam-column">
+                    <div className="ourTeam-card">
                       <img src={avatar} alt="photo-1"/>
                       <h3>Employe Name</h3>
                       <h4>Role/position</h4>
                     </div>
                 </div>
-                <div class="column">
-                    <div class="card">
+                <div className="ourTeam-column">
+                    <div className="ourTeam-card">
                         <img src={avatar2} alt="photo-1"/>
                         <h3>Employe Name</h3>
                         <h4>Role/position</h4>
