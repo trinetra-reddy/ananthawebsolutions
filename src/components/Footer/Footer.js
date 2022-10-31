3cbe867d6da4bdcadd6f50cf658b3ed8256989b7
Giri <gonuguntlagirish@gmail.com>
2022-11-01 01:39:01 +0530

    commited the footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,61 +1,46 @@
 import React from "react";
 import "./Footer.scss";
+import footer_logo from '../../images/anantha_logo.jpg';
 
 const Footer = () => {
   return (
-    <>
-      <div className="footer-main">
-        <ul className="division1">
-          <li className="logo">Anantha</li>
-          <li className="para">
-            {" "}
-            Our business accounting packages are tailored to suit your
-            individual financial requirements and unique business needs.
-          </li>
-          <li className="copyright">(c) Anantha,2022.All Rights Reserved. </li>
-        </ul>
-        <ul className="division2">
-          <li className="link">links</li>
-          <li>ABOUT US</li>
-          <li>BLOG</li>
-          <li>CONTACT US</li>
-          <li>IT DEVELOPERS</li>
-        </ul>
-        <ul className="division3">
-          <li className="recent">recent news</li>
-          <li className="text">
-            5 Incredibly Useful Water Tips For Small Businesses
-            <div className="foot">May,2022</div>
-          </li>
-          <li>
-            Little Known Ways To Rid Yourself Of Banana
-            <div className="foot">May,2022</div>
-          </li>
-        </ul>
-        <ul>
-          <li className="news">newsletter</li>
-          <li>
-            If you would like more information on how call our consultants could
-            help your business, contact us today.
-          </li>
-          <form
-            id="fs-frm"
-            name="simple-contact-form"
-            accept-charset="utf-8"
-            action="https://formspree.io/f/xlevpvdb"
-            method="post"
-          >
-            <input
-              type="email"
-              name="_replyto"
-              id="email-address"
-              placeholder="E-mail"
-              required=""
-            ></input>
-          </form>
-        </ul>
+    <div className="footer-main-container">
+    <div className="footer-container-1" style="flex-grow: 3">
+      <img src={footer_logo} className="footer_logo" alt="logo"/>
+      <h1><br>Lorem ipsum dolor sit amet consectetur,</br>
+      <br>adipisicing elit.Suscipit autem praesentium </br>
+      <br>dolorem temporibus odiomodi reiciendis,lauda</br>
+      <br>ntium, nihiltempore amet qui numquam mollitia</br>
+      </h1>
+      <h2>(c) ananthawebsolutions,2022. All Rights Reserved.</h2>
+    </div>
+    <div className="footer-container-2" style="flex-grow: 2">
+      <h1>LINKS</h1>
+      <h2>ABOUT US</h2>
+      <h3>BLOG</h3>
+      <h4>CONTACT US</h4>
+      <h5>SHOP</h5>
+      <h6>IT DEVELOPERS</h6>
+    </div>
+    <div className="footer-container-3" style="flex-grow: 2">
+      <h1>recent news</h1>
+      <h2>Best web development service <br>award</br></h2>
+      <h3>SEPTEMBER 30, 2022</h3>
+      <h4>Best service provider award</h4>
+      <h5>SEPTEMBER 30, 2022</h5>
+    </div>
+    <div className="footer-container-4" style="flex-grow: 3">
+      <h1>newsletter</h1>
+      <h2>if you would like to get more information about<br> us and get from down below</br></h2>
+      <div className="footer-email">
+        <form id="fs-frm" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/f/xlevpvdb"
+          method="post">
+          <input type="email" maxlength="50" required placeholder="E-mail"/>
+          <button class="button">Subscribe</button>
+        </form>
       </div>
-    </>
+    </div>
+  </div>
   );
 };
 
