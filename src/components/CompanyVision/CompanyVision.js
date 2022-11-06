913f97946a7d4396d854c8219f5cc0990d85e4c9
AmruthaAnumula <ammuamrutha35423@gmail.com>
2022-11-06 22:20:55 +0530

    changes commited

--- src/components/CompanyVision/CompanyVision.js
+++ src/components/CompanyVision/CompanyVision.js
@@ -9,8 +9,7 @@ const CompanyVision=() =>{
                <img src={companyVisionImg} alt="visionImage"></img>
                <div className="vision-content">
                    <h3>COMPANY VISION</h3> 
-                       <div className="vision-sub-content">We aim to be a leading contractor in Egypt by focusing on the timely implementation of our projects and meeting the expectations and requirements of our customers at the highest possible level.</div>
-                    
+                       <p className="vision-sub-content">We aim to be a leading contractor in Egypt by focusing on the timely implementation of our projects and meeting the expectations and requirements of our customers at the highest possible level.</p>
                </div>
             </div>
             
