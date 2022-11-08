5f1352d4e6a1504c1ba29cf85044eef92bf582e5
RaaviHaritha <haritha5624@gmail.com>
2022-11-08 18:15:50 +0530

    Contact Us Component created

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Footer, Header } from "../../components";
+import { Footer,Contactus,Header } from "../../components";
 import { CONSTANTS } from "../../utils/constants";
 import './Contact.scss';
 
@@ -11,6 +11,7 @@ const Contact = () => {
             <div>
                 <span>Contact Page!!</span> Link to <Link to={CONSTANTS.ROUTES.ABOUT_PAGE}>About Us</Link>
             </div>
+            <Contactus/>
             <Footer />
         </div>
     );
