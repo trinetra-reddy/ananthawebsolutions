ba2b972457c44903dcd4d038edd013d7da73a0e8
AmruthaAnumula <ammuamrutha35423@gmail.com>
2022-11-09 20:47:55 +0530

    changes in About-us page are committed.

--- src/components/Quality/Quality.js
+++ src/components/Quality/Quality.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "./Quality.scss";
 import sideimage from "./img.png";
 import meetUps from "../../icons/Meetups.png";
@@ -6,55 +6,68 @@ import target from "../../icons/Target.png";
 import customer from "../../icons/Customer.png";
 import marketing from "../../icons/Marketing.png";
 
+const Quality = () => {
+  return (
+    <>
+      <div className="quality-main">
+        <div className="quality-left">
+          <div className="quality-heading">ABOUT US</div>
+          <div className="quality-sub-heading-1">
+               We Want Your Application
+          </div>
+          <div className="quality-sub-heading-1">
+               to Have Best Quality Possible
+          </div>
+          <div className="quality-card">
+            <div className="quality-block">
+              <img
+                className="quality-icons"
+                src={customer}
+                alt="customerIcon"
+              />
 
-
-const Quality=()=>{
-    return(
-        <>
-            <div className="quality-main">
-                <div className="quality-left">
-                    <div className="quality-heading">
-                        ABOUT US
-                    </div>
-                    <div className="quality-sub-heading">
-                        <span>We Want Your Application<br></br> to Have Best Quality Possible</span>
-                    </div>
-                    <div className="quality-card">
-                        <div className="quality-block">
-                        <img className="quality-icons" src={customer} alt="customerIcon" /> 
-                        <br></br>                         
-                            Business-Oriented Software
-                            <p id="quality-p">Sed ultricies eleifend eleifend.Sed tincidunt lectus justo.</p>
-                        </div>
-                        <div class="quality-block">
-                        <img className="quality-icons" src={target} alt="targetIcon" />     
-                        <br></br>                         
-                            Excellent Price-Quality Ratio
-                            <p id="quality-p">Sed ultricies eleifend eleifend.Sed tincidunt lectus justo.</p>
-                        </div>
-                    </div>
-                    <div class="quality-card">
-                        <div class="quality-block">
-                        <img className="quality-icons" src={meetUps} alt="meetUpIcon" />
-                        <br></br>    
-                        Meetups for Developers                                    
-                        <p id="quality-p">Sed ultricies eleifend eleifend.Sed tincidunt lectus justo.</p>
-                        </div>
-                        <div className="quality-block">
-                        <img className="quality-icons" src={marketing} alt="MarketingIcon" /> 
-                        <br></br>                             
-                        Agile Responsible Development
-                        <p id="quality-p">Sed ultricies eleifend eleifend.Sed tincidunt lectus justo.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="quality-right">
-                    <img src={sideimage} alt="image" />
-                </div>
+              <h4 id="quality-h4">Business-Oriented Software</h4>
+              <p id="quality-p">
+                Sed ultricies eleifend eleifend.Sed tincidunt lectus justo.
+              </p>
             </div>
-        </>
-    );
+            <div class="quality-block">
+              <img className="quality-icons" src={target} alt="targetIcon" />
+              <h4 id="quality-h4">Excellent Price-Quality Ratio</h4>
+
+              <p id="quality-p">
+                Sed ultricies eleifend eleifend.Sed tincidunt lectus justo.
+              </p>
+            </div>
+          </div>
+          <div class="quality-card">
+            <div class="quality-block">
+              <img className="quality-icons" src={meetUps} alt="meetUpIcon" />
+              <h4 id="quality-h4">Meetups for Developers </h4>
+              <p id="quality-p">
+                Sed ultricies eleifend eleifend.Sed tincidunt lectus justo.
+              </p>
+            </div>
+            <div className="quality-block">
+              <img
+                className="quality-icons"
+                src={marketing}
+                alt="MarketingIcon"
+              />
+              <h4 id="quality-h4">Agile Responsible Development</h4>
+              <p id="quality-p">
+                Sed ultricies eleifend eleifend.Sed tincidunt lectus justo.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="quality-right">
+          <img className="quality-img" src={sideimage} alt="image" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Quality;
