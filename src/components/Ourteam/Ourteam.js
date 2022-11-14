a4ad217ed765868ad16e107076be0fe2cd8626fe
Giri <gonuguntlagirish@gmail.com>
2022-11-14 15:50:54 +0530

    commited code

--- src/components/Ourteam/Ourteam.js
+++ src/components/Ourteam/Ourteam.js
@@ -71,6 +71,7 @@ const Ourteam = () => {
             </ul>
             <img src={avatar} alt="photo-1" />
             <h3>Giridhar</h3>
+            <hr class="center-ball"></hr>
             <h4>Web & Posters Developer</h4>
           </div>
         </div>
