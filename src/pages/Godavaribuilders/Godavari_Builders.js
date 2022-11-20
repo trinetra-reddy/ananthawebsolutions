a6e335bd97f9b5f0f4202ffe53fbf58b92d18a8b
Siri K <sirichowdary874@gmail.com>
2022-11-20 15:12:26 +0530

    created godavaribuilders page

---
+++ src/pages/Godavaribuilders/Godavari_Builders.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Footer, Header } from "../../components";
+import { CONSTANTS } from "../../utils/constants";
+import './Godavari_Builders.scss';
+
+const Godavari_Builders = () => {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    );
+};
+
+export default Godavari_Builders;
