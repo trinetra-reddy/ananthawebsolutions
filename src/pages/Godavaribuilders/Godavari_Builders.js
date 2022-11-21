965ae6efee18e21c8579704205c299e7a9012d1f
AmruthaAnumula <ammuamrutha35423@gmail.com>
2022-11-21 19:19:58 +0530

    changes in happy clients component are commited.

--- src/pages/Godavaribuilders/Godavari_Builders.js
+++ src/pages/Godavaribuilders/Godavari_Builders.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Footer, Header,Properties,Propertiesfooter } from "../../components";
+import { Footer, Header,Properties,HappyClients,Propertiesfooter } from "../../components";
 import { CONSTANTS } from "../../utils/constants";
 import './Godavari_Builders.scss';
 
@@ -10,6 +10,7 @@ const Godavari_Builders = () => {
         <div> 
             <Header />
             <Properties />
+            <HappyClients />
             <Propertiesfooter />
             <Footer />
         </div>
