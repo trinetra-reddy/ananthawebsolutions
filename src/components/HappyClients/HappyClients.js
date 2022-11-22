5b70d03320e14f6b0ca62f3f3b32b8ad0fc3dab0
AmruthaAnumula <ammuamrutha35423@gmail.com>
2022-11-22 20:31:29 +0530

    changes in HappyClients components are commited.

--- src/components/HappyClients/HappyClients.js
+++ src/components/HappyClients/HappyClients.js
@@ -1,27 +1,27 @@
 import React from "react";
-import vectorImg from "./underline.png";
-import client1 from "./happyboy.png";
-import client2 from "./happygirl.png";
+import vectorhappyClientsImg from "./underline.png";
+import happyclient1 from "./happyboy.png";
+import happyclient2 from "./happygirl.png";
 import "./HappyClients.scss";
 
 const HappyClients = () => {
   return (
     <>
-      <div className="happy-main">
-        <div className="happy-heading">Happy Clients</div>
+      <div className="happyclients-main">
+        <div className="happyclients-heading">Happy Clients</div>
         <img
-          className="happy-vector"
-          src={vectorImg}
-          alt="Happy-vector-img"
+          className="happyclients-vector"
+          src={vectorhappyClientsImg}
+          alt="HappyClients-vector-img"
         ></img>
         <div className="happy-clients">
           <div className="client1">
             <div className="clientImg">
-              <img className="happy-client-img" src={client1} alt="Client1"></img>
+              <img className="happy-client-img" src={happyclient1} alt="Client1"></img>
             </div>
-            <div className="client-details">
-              <div className="client-Name">Krishna Kanth</div>
-              <p className="client-content">
+            <div className="happyclients-details">
+              <div className="happyclients-Name">Krishna Kanth</div>
+              <p className="happyclients-content">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
                 enim ad minim veniam,quis nostrud exercitation.
@@ -30,11 +30,11 @@ const HappyClients = () => {
             </div>
             <div className="client2">
               <div className="clientImg">
-                <img className="happy-client-img" src={client2} alt="Client2"></img>
+                <img className="happy-client-img" src={happyclient2} alt="Client2"></img>
               </div>
-              <div className="client-details">
-                <div className="client-Name">Ramika sayn</div>
-                <p className="client-content">
+              <div className="happyclients-details">
+                <div className="happyclients-Name">Ramika sayn</div>
+                <p className="happyclients-content">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna
                   aliqua.Ut enim ad minim veniam,quis nostrud exercitation.
@@ -43,14 +43,14 @@ const HappyClients = () => {
             </div>
           </div>
           <div className="happy2">
-            <div className="happy-video">
-            <embed className="client-video" src="https://www.youtube.com/embed/9MfX6D9jXwo" autoplay controls ></embed>
+            <div className="happyclients-video">
+            <embed className="happyclients-video" src="https://www.youtube.com/embed/9MfX6D9jXwo" autoplay controls ></embed>
             </div>
-            <div className="happy-video">
-            <embed className="client-video" src="https://www.youtube.com/embed/9MfX6D9jXwo" autoplay controls  ></embed>
+            <div className="happyclients-video">
+            <embed className="happyclients-video" src="https://www.youtube.com/embed/9MfX6D9jXwo" autoplay controls  ></embed>
             </div>
-            <div className="happy-video">
-            <embed className="client-video" src="https://www.youtube.com/embed/9MfX6D9jXwo" autoplay controls ></embed>
+            <div className="happyclients-video">
+            <embed className="happyclients-video" src="https://www.youtube.com/embed/9MfX6D9jXwo" autoplay controls ></embed>
             </div>
           </div>
         </div>
