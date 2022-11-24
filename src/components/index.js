7030f3cbfebea147f5391a4956720074fd1e40e2
Siri K <sirichowdary874@gmail.com>
2022-11-24 19:18:55 +0530

    commt

--- src/components/index.js
+++ src/components/index.js
@@ -14,15 +14,6 @@ import Companymission from "./Companymission/Companymission";
 import Properties from "./Properties/Properties";
 import Propertiesfooter from "./Propertiesfooter/Propertiesfooter";
 import HappyClients from "./HappyClients/HappyClients";
-<<<<<<< HEAD
 import Floorplan from "./Floorplan/Floorplan";
 
 export { Header, Footer,Banner,Tech, Whychooseus,Quality,About ,GetTouch,Ourteam,CompanyCulture,CompanyVision,Contactus,Companymission,Properties,Propertiesfooter,HappyClients,Floorplan};
-=======
-import Specifications from "./Specifications/Specifications";
-import Godavariheader from "./Godavariheader/Godavariheader";
-import Improvingpeople from "./Improvingpeople/Improvingpeople";
-import Godavariaboutus from "./Godavariaboutus/Godavariaboutus";
-
-export { Header, Footer,Banner,Tech, Whychooseus,Quality,About ,GetTouch,Ourteam,CompanyCulture,CompanyVision,Contactus,Companymission,Properties,Propertiesfooter,HappyClients,Godavariheader,Specifications,Improvingpeople,Godavariaboutus};
->>>>>>> d38150a35dbc5a58cabe441c79832b1a79ab6f13
