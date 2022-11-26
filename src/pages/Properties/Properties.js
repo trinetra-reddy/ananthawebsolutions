652f6a8af75b2d7a250155b6c09e80dbd366b3fb
Siri K <sirichowdary874@gmail.com>
2022-11-26 12:53:26 +0530

    made changes in floorplan

--- src/pages/Properties/Properties.js
+++ src/pages/Properties/Properties.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Ourculture,Floorplan,Propertiesfooter } from "../../components";
-import { Floorplan,Godavariheader,Propertiesfooter, Yourdreamhouse,Specifications } from "../../components";
+import {Godavariheader, Floorplan,Propertiesfooter } from "../../components";
+import {  Yourdreamhouse,Specifications } from "../../components";
 import { CONSTANTS } from "../../utils/constants";
 import './Properties.scss';
-import image from './image.svg';
+
 
 
 const Properties = () => {
-    const ourCultureDes = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa';
-    const ourVisionDes = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa'
     
     return (
         <div> 
-            <Ourculture name="our W" description={ourCultureDes} linkeddescription={ourCultureDes} imgPath={image}/>
-            <Ourculture name="Our V" description={ourVisionDes} linkeddescription={ourCultureDes} imgPath={image}/>
+            <Godavariheader />
             <Floorplan />
             <Propertiesfooter />
         </div>
--- src/pages/Properties2/Properties2.js
+++ src/pages/Properties2/Properties2.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React from 'react';
 import { Link } from "react-router-dom";
-import { Godavariheader, Propertiesfooter } from "../../components";
+import { Godavariheader,Ourculture, Propertiesfooter } from "../../components";
 import { CONSTANTS } from "../../utils/constants";
 import './Properties2.scss';
-
+import image from './image.svg';
 
 const Properties2 = () => {
+    const ourCultureDes = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa';
+    const ourVisionDes = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa'
+    
     return (
         <div>
             <Godavariheader /> 
+            <Ourculture name="our W" description={ourCultureDes} linkeddescription={ourCultureDes} imgPath={image}/>
+            <Ourculture name="Our V" description={ourVisionDes} linkeddescription={ourCultureDes} imgPath={image}/>
+            
             <Propertiesfooter />
         </div>
     );
