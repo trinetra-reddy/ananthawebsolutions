7ca58afe597c22aedb277ea3191b0563fc1c41f4
Giri <gonuguntlagirish@gmail.com>
2022-11-28 14:27:38 +0530

    commited code

--- src/pages/Properties/Properties.js
+++ src/pages/Properties/Properties.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {Godavariheader, Floorplan,Propertiesfooter } from "../../components";
-import {  Yourdreamhouse,Specifications } from "../../components";
+import {Godavariheader,Yourdreamhouse,Floorplan,Propertiesfooter,Specifications } from "../../components";
 import { CONSTANTS } from "../../utils/constants";
 import './Properties.scss';
 
@@ -12,6 +11,7 @@ const Properties = () => {
     return (
         <div> 
             <Godavariheader />
+            <Yourdreamhouse />
             <Floorplan />
             <Propertiesfooter />
         </div>
