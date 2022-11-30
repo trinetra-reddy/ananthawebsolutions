21113ee251bcc4338ef85e80531dbfb8feda82b7
Raavi Haritha <haritha4682@gmail.com>
2022-11-30 18:30:34 +0530

    contact us modified

--- src/components/Contactus/Contactus.js
+++ src/components/Contactus/Contactus.js
@@ -69,17 +69,13 @@ const Contactus = () => {
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1750.1688261186528!2d77.53443313587825!3d14.6469047904435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb135a1a082c657%3A0x1924ed05e2103a21!2sAlamur%2C%20Kakkalapalle%20Rural%2C%20Andhra%20Pradesh%20515002%2C%20India!5e0!3m2!1sen!2sus!4v1668018257685!5m2!1sen!2sus"></iframe>
           </div>
           <div className="Contactus-footer-wrapper">
-            <div
-              className="Contactus-address-wrapper"
-            >
+            <div className="Contactus-address-wrapper">
               <h6 className="Contactus-address-headers">COMPANY ADDRESS</h6>
               <p className="Contactus-location-content">
                 ThemeForest,Envato HQ 24 Fifth st., Los Angeles, USA
               </p>
             </div>
-            <div
-              className="Contactus-address-wrapper"
-            >
+            <div className="Contactus-address-wrapper">
               <h6 className="Contactus-address-headers">COMPANY PHONES</h6>
               <img
                 className="Contactus-icons"
@@ -92,9 +88,7 @@ const Contactus = () => {
                 <li>1234-1234-1234</li>
               </ul>
             </div>
-            <div
-              className="Contactus-address-wrapper"
-            >
+            <div className="Contactus-address-wrapper">
               <h6 className="Contactus-address-headers">COMPANY EMAILS</h6>
               <img
                 className="Contactus-icons"
